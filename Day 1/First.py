1e19d983621e836478f3877a82e6abdaa389567b
Anton From <503142243@ge.com>
2020-12-03 11:43:00 +0100

    Day 1 part 2

--- Day 1/First.py
+++ Day 1/First.py
@@ -4,15 +4,20 @@ with open('Day 1/Indata.txt','r') as file:
 
     for i in range(len(list)):
         for j in range(i, len(list)):
-            # print("i: {0}, j: {1}".format(i,j))
-            if i == j:
+            for k in range(j, len(list)):
+
+                if i == j or i == k or j == k:
+                    continue
+                if list[i] + list[j] + list[k] == 2020:
+                    print("list[{}] = {}".format(i,list[i]))
+                    print("list[{}] = {}".format(j,list[j]))
+                    print("list[{}] = {}".format(k,list[k]))
+                    print("sum: "+str((list[i]+list[j]+list[k])))
+                    print("times: "+str((list[i]*list[j]*list[k])))
+                    break
+            else:
                 continue
-            if list[i] + list[j] == 2020:
-                print("list[{}] = {}".format(i,list[i]))
-                print("list[{}] = {}".format(j,list[j]))
-                print("sum: "+str((list[i]+list[j])))
-                print("times: "+str((list[i]*list[j])))
-                break
+            break
         else:
             continue
         break
