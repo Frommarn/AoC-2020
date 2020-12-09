046791866a8e60f2e862ac270c1b4ae1ea9abcd7
Anton From <503142243@ge.com>
2020-12-09 12:51:10 +0100

    Day 8 part 2

--- Day 8/Eighth.py
+++ Day 8/Eighth.py
@@ -13,26 +13,56 @@ for rawDataLine in rawDataLines:
     operation, argument = rawDataLine.split(' ')
     instructions.append(InstructionTuple(operation, int(argument)))
 
-# Run the simulator and track which instructions have been executed
-accumulator = 0
-visitedInstructions = [0] * len(instructions)
+isSuccess = False
+for index in range(len(instructions)):
+    if instructions[index].operation not in ['nop', 'jmp']:
+        continue
 
-nextInstructionIndex = 0
-while True:
-    # If we have already executed the next instruction, stop the simulation
-    if visitedInstructions[nextInstructionIndex] == 1:
-        break
+    # Run the simulator and track which instructions have been executed
+    accumulator = 0
+    visitedInstructions = [0] * len(instructions)
+    modifiedInstructions = instructions.copy()
+
+    # Modify nop/jmp instruction
+    tmp = modifiedInstructions[index]
+    if tmp.operation == 'nop':
+        modifiedInstructions[index] = InstructionTuple('jmp', tmp.argument)
+    elif tmp.operation == 'jmp':
+        modifiedInstructions[index] = InstructionTuple('nop', tmp.argument)
     else:
-        nextInstruction = instructions[nextInstructionIndex]
-        visitedInstructions[nextInstructionIndex] = 1
-        if nextInstruction.operation == 'acc':
-            accumulator += nextInstruction.argument
-            nextInstructionIndex += 1
-        elif nextInstruction.operation == 'jmp':
-            nextInstructionIndex += nextInstruction.argument
-        elif nextInstruction.operation == 'nop':
-            nextInstructionIndex += 1
-        else:
-            raise Exception('The impossible happened!')
+        raise Exception('The impossible happened!')
 
-print('accumulator at start of infinite loop: ' + str(accumulator))
+    nextInstructionIndex = 0
+    while True:
+        # If we try to execute exactly one step outside the instruction list, we fixed the program!
+        if nextInstructionIndex == len(modifiedInstructions):
+            isSuccess = True
+            print('Success!')
+            break
+        # If we try to execute more than one step outside the instruction list, we broke something
+        elif nextInstructionIndex > len(modifiedInstructions):
+            print('We overshot!')
+            break
+        # If we try to execute on a negative index on the instruction list, we broke something
+        elif nextInstructionIndex < 0:
+            print('Negative index!')
+            break
+        # If we have already executed the next instruction, stop the simulation
+        elif visitedInstructions[nextInstructionIndex] == 1:
+            break
+        else:
+            nextInstruction = modifiedInstructions[nextInstructionIndex]
+            visitedInstructions[nextInstructionIndex] = 1
+            if nextInstruction.operation == 'acc':
+                accumulator += nextInstruction.argument
+                nextInstructionIndex += 1
+            elif nextInstruction.operation == 'jmp':
+                nextInstructionIndex += nextInstruction.argument
+            elif nextInstruction.operation == 'nop':
+                nextInstructionIndex += 1
+            else:
+                raise Exception('The impossible happened!')
+    
+    if isSuccess:
+        print('accumulator at start of infinite loop: ' + str(accumulator))
+        break
